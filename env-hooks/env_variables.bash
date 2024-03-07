53362dc205b75581f14cf10603a9579c38590d55
Victor on ROS2 VM <victor.dubois@free.fr>
2024-03-07 22:08:02 +0100

    ROS2 conversion

--- env-hooks/env_variables.bash
+++ env-hooks/env_variables.bash
@@ -1,3 +1,5 @@
-#!/bin/sh
-PACKAGE_PATH="$(rospack find eurobot2020_gazebo)"
-export GAZEBO_MODEL_PATH="$PACKAGE_PATH/models:${GAZEBO_MODEL_PATH}"
+# $AMENT_CURRENT_PREFIX is replaced with /opt/ros/<distro>/share/ on clean builds, that's why using $COLCON_CURRENT_PREFIX instead.
+ament_prepend_unique_value GAZEBO_MODEL_PATH "$COLCON_CURRENT_PREFIX/share/@PROJECT_NAME@/models"
+ament_prepend_unique_value GAZEBO_RESOURCE_PATH "$COLCON_CURRENT_PREFIX/share/@PROJECT_NAME@/worlds"
+ament_prepend_unique_value GAZEBO_RESOURCE_PATH "$COLCON_CURRENT_PREFIX/share/@PROJECT_NAME@/media"
+ament_prepend_unique_value GAZEBO_PLUGIN_PATH "$COLCON_CURRENT_PREFIX/lib"
